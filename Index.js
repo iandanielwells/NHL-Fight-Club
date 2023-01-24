f77058764840e73d2c3353e478a17b9aeaef3503
tecon7 <teddyecon@gmail.com>
2023-01-24 15:04:53 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,12 +1,50 @@
 fetch('http://localhost:3000/logos')
 .then(resp => resp.json())
 .then(teamLogos => {
-    renderTeamLogos(teamLogos)
+    const logosNav = document.querySelector('#Logosgohere');
+    teamLogos.forEach(logo => {
+        const img = document.createElement('img');
+        img.src = logo.image;
+        logosNav.append(img);
+        img.addEventListener('click', (event) => {
+            const showData = document.getElementById('show-data');
+            showData.textContent = "";
+            showData.append(logo.name);
+            fetch('https://statsapi.web.nhl.com/api/v1/teams?expand=team.stats')
+            .then(response => response.json())
+            .then(teamData => {
+                console.log(teamData);
+                if (showData.textContent == teamData.teams.name
+                    //also tried logo.name === teamData.teams.name
+                    ) {
+                    showData.append(teamData.teams.venue.name)
+                } else {showData.append('  something')}
+            })
+        })
+    })
 })
-function renderTeamLogos(teamLogos) {
-    teamLogos.forEach((logo) => {
-        const logoImages = document.createElement('img')
-        logoImages.src = logo.image
-        document.querySelector('#Logosgohere').append(logoImages)
+function setUpFightForm() {
+    const fightForm = document.querySelector('#fight-form')
+    fightForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        
+        const userTeam = document.querySelector('#teams-involved').value
+        const uploadTeam = document.querySelector('#new-teams')
+        uploadTeam.innerText = userTeam
+
+        const userName = document.querySelector('#new-name').value 
+        const uploadName = document.querySelector('#new-winner')
+        uploadName.innerText = userName
+
+        const userImage = document.querySelector('#new-image').value
+        const uploadImage = document.querySelector('#new-upload')
+        uploadImage.src = userImage
+
+        const userRating = document.querySelector('#select-rating').value
+        const uploadRating = document.querySelector('#rating-display')
+        uploadRating.innerHTML = userRating
+
+    fightForm.reset()
     })
 }
+setUpFightForm()
